hifi/keepassxc

commit 9477437256c8b34d0bf124c07abf7e08690dd824

:bug: Fix for the issue #108: Add a scrollbar in the AddEntry window when on "small" screen

src/gui/EditWidget.cpp
@@ -17,6 +17,7 @@
 
 #include "EditWidget.h"
 #include "ui_EditWidget.h"
+#include <QScrollArea>
 
 #include "core/FilePath.h"
 
@@ -47,7 +48,17 @@ EditWidget::~EditWidget()
 
 void EditWidget::addPage(const QString& labelText, const QIcon& icon, QWidget* widget)
 {
-    m_ui->stackedWidget->addWidget(widget);
+    /*
+     * Instead of just adding a widget we're going to wrap it into a scroll area. It will automatically show
+     * scrollbars when the widget cannot fit into the page. This approach prevents the main window of the application
+     * from automatic resizing and it now should be able to fit into a user's monitor even if the monitor is only 768
+     * pixels high.
+     */
+    QScrollArea* scrollArea = new QScrollArea(m_ui->stackedWidget);
+    scrollArea->setFrameShape(QFrame::NoFrame);
+    scrollArea->setWidget(widget);
+    scrollArea->setWidgetResizable(true);
+    m_ui->stackedWidget->addWidget(scrollArea);
     m_ui->categoryList->addCategory(labelText, icon);
 }
 

commit 7f30301634678427b1b5df66d2debb6fe3814b50

Fix hiding entry edit pages
Fixes regression caused by 9477437256c8b34d0bf124c07abf7e08690dd824

src/gui/EditWidget.cpp
@@ -70,7 +70,16 @@ void EditWidget::addPage(const QString& labelText, const QIcon& icon, QWidget* w
 
 void EditWidget::setPageHidden(QWidget* widget, bool hidden)
 {
-    int index = m_ui->stackedWidget->indexOf(widget);
+    int index = -1;
+
+    for (int i = 0; i < m_ui->stackedWidget->count(); i++) {
+        auto* scrollArea = qobject_cast<QScrollArea*>(m_ui->stackedWidget->widget(i));
+        if (scrollArea != nullptr && scrollArea->widget() == widget) {
+            index = i;
+            break;
+        }
+    }
+
     if (index != -1) {
         m_ui->categoryList->setCategoryHidden(index, hidden);
     }